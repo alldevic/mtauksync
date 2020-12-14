2226fc0f03fd486ce5183fb697c9fb2c1fd1ecad
Nikolay Bely <belyynikolay@gmail.com>
2020-12-14 18:47:49 +0700

    Fix quotes

--- initcmds/management/commands/inittasks.py
+++ initcmds/management/commands/inittasks.py
@@ -9,38 +9,38 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         # Create containers task
         try:
-            Schedule.objects.get(name="auk_containers")
+            Schedule.objects.get(name='auk_containers')
         except ObjectDoesNotExist:
-            self.stdout.write(self.style.SUCCESS("Create auk_containers"))
+            self.stdout.write(self.style.SUCCESS('Create auk_containers'))
             Schedule.objects.create(
-                name="auk_containers",
+                name='auk_containers',
                 func='django.core.management.call_command',
-                args='getcontainers',
+                args='"getcontainers"',
                 schedule_type=Schedule.MINUTES,
                 minutes=5
             )
 
         # Create platform task
         try:
-            Schedule.objects.get(name="auk_platforms")
+            Schedule.objects.get(name='auk_platforms')
         except ObjectDoesNotExist:
-            self.stdout.write(self.style.SUCCESS("Create auk_platforms"))
+            self.stdout.write(self.style.SUCCESS('Create auk_platforms'))
             Schedule.objects.create(
-                name="auk_platforms",
+                name='auk_platforms',
                 func='django.core.management.call_command',
-                args='getplatforms',
+                args='"getplatforms"',
                 schedule_type=Schedule.MINUTES,
                 minutes=5
             )
         # Create mt task
         try:
-            Schedule.objects.get(name="mt_post")
+            Schedule.objects.get(name='mt_post')
         except ObjectDoesNotExist:
-            self.stdout.write(self.style.SUCCESS("Create mt_post"))
+            self.stdout.write(self.style.SUCCESS('Create mt_post'))
             Schedule.objects.create(
-                name="mt_post",
+                name='mt_post',
                 func='django.core.management.call_command',
-                args='postdelta',
+                args='"postdelta"',
                 schedule_type=Schedule.MINUTES,
                 minutes=3
             )
