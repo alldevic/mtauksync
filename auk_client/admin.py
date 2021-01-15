3434bf6f7106d40d3681870dbe005dae13c89a3a
Nikolay Bely <belyynikolay@gmail.com>
2021-01-15 21:18:10 +0700

    Add link to auk platorm for admin

--- auk_client/admin.py
+++ auk_client/admin.py
@@ -1,14 +1,23 @@
 from django.contrib import admin
-from auk_client.models import (Container, Platform)
+from django.utils.html import escape, mark_safe
+
+from auk_client.models import Container, Platform
 
 
 @admin.register(Container)
 class ContainerAdmin(admin.ModelAdmin):
-    list_display = ('auk_id', 'mt_id', 'created', 'updated',)
+    list_display = ('id', 'auk_id', 'mt_id', 'created', 'updated',)
     search_fields = ('auk_id', 'mt_id',)
 
 
 @admin.register(Platform)
 class PlatformAdmin(admin.ModelAdmin):
-    list_display = ('auk_id', 'mt_id', 'created', 'updated',)
+    list_display = ('id', 'auk_id_link', 'mt_id', 'created', 'updated',)
     search_fields = ('auk_id', 'mt_id',)
+
+    def auk_id_link(self, obj: Platform):
+        if obj:
+            return mark_safe(f'<a href="http://auk.kuzro.ru/platform/{obj.auk_id}">\
+                {escape(obj.auk_id)}</a>')
+        else:
+            return None
