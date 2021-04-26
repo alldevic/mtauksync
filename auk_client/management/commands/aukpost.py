573872a3b6c4c1359556a30d0ca7b45e47349aee
Nikolay Bely <belyynikolay@gmail.com>
2021-04-26 18:40:50 +0000

    add task states

--- auk_client/management/commands/aukpost.py
+++ auk_client/management/commands/aukpost.py
@@ -27,7 +27,23 @@ class Command(BaseCommand):
                     days=int(settings.FIRST_RUN_DAYS)
                 )
             )
-        self.stdout.write(self.style.SUCCESS(f'Last task: {last_task}'))
+
+        self.stdout.write(self.style.SUCCESS(f'Last task: {last_task}, {last_task.status}'))
+        
+        if last_task.status is "p":
+            self.stdout.write(self.style.SUCCESS(
+                "--- Task already in progress ---"))
+            elapsed = time.time()
+            self.stdout.write(self.style.SUCCESS(
+                "--- Total %s seconds ---" % (elapsed - start_time)))
+            return 
+
+        if last_task.status in ["s", "f"]:
+            last_task = TaskModel.objects.create(
+                taskname="auk_post",
+                lastrunned=start_date
+            )
+
         platforms = [x for x in replicationauk.objects.using(
             "mtdb").all().filter(owner="mt", essence="platform")]
         containers = [x for x in replicationauk.objects.using(
@@ -41,53 +57,61 @@ class Command(BaseCommand):
         s.auth = LogPassAuth("http://apiauk.kuzro.ru/token/login/",
                              settings.AUK_LOGIN, settings.AUK_PASS)
 
-        for x in platforms:
-            res = None
-            if x.action == "insert":
-                res = s.post("http://apiauk.kuzro.ru/platforms/", json=x.attribute)
-            elif x.action == "update":
-                res = s.patch(
-                    f"http://apiauk.kuzro.ru/platforms/{x.id_auk}/",
-                    json=x.attribute)
-            elif x.action == "delete":
-                print("Not implemented")
+        last_task.status = "p"
+        last_task.save()
 
-            if res:
-                if res.status_code != 200:
-                    print(f"{res.status_code} -- {res.content}")
+        try:
+            for x in platforms:
+                res = None
+                if x.action == "insert":
+                    res = s.post("http://apiauk.kuzro.ru/platforms/", json=x.attribute)
+                elif x.action == "update":
+                    res = s.patch(
+                        f"http://apiauk.kuzro.ru/platforms/{x.id_auk}/",
+                        json=x.attribute)
+                elif x.action == "delete":
+                    print("Not implemented")
 
-            replicationauk.objects.using("mtdb").filter(id=x.id).delete()
+                if res:
+                    if res.status_code != 200:
+                        print(f"{res.status_code} -- {res.content}")
 
-        for x in containers:
-            res = None
-            if x.action == "create":
-                res = s.post("http://apiauk.kuzro.ru/containers/", json=x.attribute)
-            elif x.action == "update":
-                res = s.patch(
-                    f"http://apiauk.kuzro.ru/containers/{x.id_auk}/",
-                    json=x.attribute)
-            elif x.action == "delete":
-                print("Not implemented")
+                replicationauk.objects.using("mtdb").filter(id=x.id).delete()
 
-            if res:
-                if res.status_code != 200:
-                    print(f"{res.status_code} -- {res.content}")
+            for x in containers:
+                res = None
+                if x.action == "create":
+                    res = s.post("http://apiauk.kuzro.ru/containers/", json=x.attribute)
+                elif x.action == "update":
+                    res = s.patch(
+                        f"http://apiauk.kuzro.ru/containers/{x.id_auk}/",
+                        json=x.attribute)
+                elif x.action == "delete":
+                    print("Not implemented")
 
-            replicationauk.objects.using("mtdb").filter(id=x.id).delete()
-            replicationauk.objects.using("mtdb").filter(id=x.id).delete()
+                if res:
+                    if res.status_code != 200:
+                        print(f"{res.status_code} -- {res.content}")
 
-        elapsed = time.time()
-        self.stdout.write(self.style.SUCCESS(
-            "--- %s seconds ---" % (elapsed - start_time)))
+                replicationauk.objects.using("mtdb").filter(id=x.id).delete()
+                replicationauk.objects.using("mtdb").filter(id=x.id).delete()
 
-        TaskModel.objects.create(
-            taskname="auk_post",
-            lastrunned=start_date
-        )
+            elapsed = time.time()
+            self.stdout.write(self.style.SUCCESS(
+                "--- %s seconds ---" % (elapsed - start_time)))
 
-        elapsed = time.time()
-        self.stdout.write(self.style.SUCCESS(
-            "--- Total %s seconds ---" % (elapsed - start_time)))
+            last_task.status = "s"
+            last_task.save()
+        except BaseException as e:
+            last_task.status = "f"
+            last_task.save()
+            msg = str("\n".join(filter(None, map(str, list(e.args)))))
+            last_task.fail = msg
+            self.stdout.writse(self.style.ERROR(msg))
+        finally:
+            elapsed = time.time()
+            self.stdout.write(self.style.SUCCESS(
+                "--- Total %s seconds ---" % (elapsed - start_time)))
 
 
 class LogPassAuth(AuthBase):
--- initcmds/models.py
+++ initcmds/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
 
+TASK_STATUS = (
+    ("c", "created"), 
+    ("p", "progress"), 
+    ("s", "success"),
+    ("f", "failed")
+)
+
 class TaskModel(models.Model):
     lastrunned = models.DateTimeField(
         "lastrunned", auto_now=False, auto_now_add=False)
     taskname = models.CharField("taskname", max_length=50)
-
+    status = models.CharField(max_length=1, choices=TASK_STATUS, default='c')
+    fail = models.TextField("fail", blank=True, null=True)
     def __str__(self) -> str:
         return f"{self.taskname} - {self.lastrunned}"
 
